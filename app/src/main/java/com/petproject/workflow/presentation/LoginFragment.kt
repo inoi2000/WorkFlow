b231fa7aee2d56a1e4fa0a9263e8e188beb630e7
inoi2000 <nikolaiklonov@yandex.ru>
2024-11-14 23:21:26 +0500

    update dependencies

--- app/src/main/java/com/petproject/workflow/presentation/LoginFragment.kt
+++ app/src/main/java/com/petproject/workflow/presentation/LoginFragment.kt
@@ -5,15 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.petproject.workflow.R
 
 class LoginFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        auth = Firebase.auth
+        return view
     }
 }
