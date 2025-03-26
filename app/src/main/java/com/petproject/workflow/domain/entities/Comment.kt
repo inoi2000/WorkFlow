b7d9f3b0724094d767e7223f96504b2f2447309b
inoi2000 <124907504+inoi2000@users.noreply.github.com>
2025-03-26 16:42:43 +0500

    Update task views (#15)
    * edit views
    
    * add vacation recycler view
    
    * update views

--- app/src/main/java/com/petproject/workflow/domain/entities/Comment.kt
+++ app/src/main/java/com/petproject/workflow/domain/entities/Comment.kt
@@ -1,9 +1,12 @@
 package com.petproject.workflow.domain.entities
 
 import java.io.File
+import java.time.LocalDate
 
 data class Comment(
     val id: String = "",
     val text: String,
+    val creation: LocalDate,
+    val task: Task? = null,
     val files: List<File>? = null
 )
--- app/src/main/java/com/petproject/workflow/presentation/viewmodels/ExecutorTaskListViewModel.kt
+++ app/src/main/java/com/petproject/workflow/presentation/viewmodels/ExecutorTaskListViewModel.kt
@@ -3,18 +3,40 @@ package com.petproject.workflow.presentation.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
 import com.petproject.workflow.domain.entities.Task
+import com.petproject.workflow.domain.entities.TaskPriority
+import com.petproject.workflow.domain.entities.TaskStatus
 import com.petproject.workflow.domain.usecases.GetAllExecutingTasksUseCase
 import kotlinx.coroutines.launch
+import java.time.LocalDate
 import javax.inject.Inject
 
 class ExecutorTaskListViewModel @Inject constructor(
     private val getAllExecutingTasksUseCase: GetAllExecutingTasksUseCase
-): ViewModel() {
+) : ViewModel() {
 
-    private val _executingTasksList = MutableLiveData<List<Task>>()
-    val executingTasksList: LiveData<List<Task>> get() = _executingTasksList
+    private val _tasksList = MutableLiveData<List<Task>>()
+
+    private val _filteredTaskList = MutableLiveData<List<Task>>()
+    val filteredTaskList: LiveData<List<Task>> get() = _filteredTaskList
+
+    val overdueTasksCount: LiveData<Int> = _tasksList.map { taskList ->
+        taskList.count { it.status == TaskStatus.FAILED }
+    }
+
+    val urgentTasksCount: LiveData<Int> = _tasksList.map { taskList ->
+        taskList.count { it.priority == TaskPriority.URGENT }
+    }
+
+    val onApprovalTasksCount: LiveData<Int> = _tasksList.map { taskList ->
+        taskList.count { it.status == TaskStatus.ON_APPROVAL }
+    }
+
+    val finishedTasksCount: LiveData<Int> = _tasksList.map { taskList ->
+        taskList.count { it.status == TaskStatus.COMPLETED }
+    }
 
     init {
         loadData()
@@ -22,7 +44,20 @@ class ExecutorTaskListViewModel @Inject constructor(
 
     fun loadData() {
         viewModelScope.launch {
-            _executingTasksList.value = getAllExecutingTasksUseCase()
+            _tasksList.value = getAllExecutingTasksUseCase()
+            filteredTaskListByDefault()
         }
+    }
+
+    fun filteredTaskListByDefault() {
+        _filteredTaskList.value = _tasksList.value
+    }
+
+    fun filteredTaskListByStatus(status: TaskStatus) {
+        _filteredTaskList.value = _tasksList.value?.filter { it.status == status }
+    }
+
+    fun filteredTaskListByPriority(priority: TaskPriority) {
+        _filteredTaskList.value = _tasksList.value?.filter { it.priority == priority }
     }
 }
