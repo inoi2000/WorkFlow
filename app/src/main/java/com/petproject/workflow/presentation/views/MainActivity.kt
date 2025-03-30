eef2774115917e9df80fa0d0b37752bbef87bfc6
inoi2000 <nikolaiklonov@yandex.ru>
2025-03-31 00:33:49 +0500

    remove refs

--- app/src/main/java/com/petproject/workflow/presentation/views/MainActivity.kt
+++ app/src/main/java/com/petproject/workflow/presentation/views/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.petproject.workflow.R
 import com.petproject.workflow.databinding.ActivityMainBinding
 
