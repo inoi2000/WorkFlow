5acfc77388cb78dbbba33be1d0a9474fb2a1ace5
inoi2000 <nikolaiklonov@yandex.ru>
2025-04-01 13:42:17 +0500

    add InspectorTaskInfoFragment

---
+++ app/src/main/java/com/petproject/workflow/presentation/viewmodels/InspectorTaskInfoViewModel.kt
@@ -0,0 +1,31 @@
+package com.petproject.workflow.presentation.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.petproject.workflow.domain.entities.Task
+import com.petproject.workflow.domain.usecases.GetInspectorTaskUseCase
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class InspectorTaskInfoViewModel @Inject constructor(
+    private val taskId: String,
+    private val getInspectorTaskUseCase: GetInspectorTaskUseCase
+): ViewModel() {
+
+    private val _inspectorTask = MutableLiveData<Task>()
+    val inspectorTask: LiveData<Task> get() = _inspectorTask
+
+    val commentsCount: String get() = inspectorTask.value?.commentsCount.toString()
+
+    init {
+        loadData()
+    }
+
+    fun loadData() {
+        viewModelScope.launch {
+            _inspectorTask.value = getInspectorTaskUseCase(taskId)
+        }
+    }
+}
