b49f14f946bf4c71266f05a72b7bc1d8067bd3e5
inoi2000 <nikolaiklonov@yandex.ru>
2025-05-23 00:35:40 +0500

    edit InspectorTaskInfoFragment

--- app/src/main/java/com/petproject/workflow/presentation/views/ExecutorTaskInfoFragment.kt
+++ app/src/main/java/com/petproject/workflow/presentation/views/ExecutorTaskInfoFragment.kt
@@ -12,6 +12,7 @@ import com.petproject.workflow.WorkFlowApplication
 import com.petproject.workflow.databinding.FragmentExecutorTaskInfoBinding
 import com.petproject.workflow.presentation.viewmodels.ExecutorTaskInfoViewModel
 import com.petproject.workflow.presentation.viewmodels.ViewModelFactory
+import com.petproject.workflow.presentation.views.adapters.EmployeeInfoViewHolder
 import javax.inject.Inject
 
 class ExecutorTaskInfoFragment : Fragment() {
@@ -50,10 +51,20 @@ class ExecutorTaskInfoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observeViewModel()
         binding.commentsCardView.setOnClickListener {
             val action = ExecutorTaskInfoFragmentDirections
                 .actionExecutingTaskInfoFragmentToTaskCommentListFragment(args.taskId)
             findNavController().navigate(action)
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.executingTask.observe(viewLifecycleOwner) {
+            it.inspector?.let { inspector ->
+                val employeeInfoViewHolder = EmployeeInfoViewHolder(binding.taskInspector)
+                employeeInfoViewHolder.bind(inspector) {}
+            }
         }
     }
 
--- app/src/main/java/com/petproject/workflow/presentation/views/InspectorTaskInfoFragment.kt
+++ app/src/main/java/com/petproject/workflow/presentation/views/InspectorTaskInfoFragment.kt
@@ -1,19 +1,69 @@
 package com.petproject.workflow.presentation.views
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.petproject.workflow.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
+import com.petproject.workflow.WorkFlowApplication
+import com.petproject.workflow.databinding.FragmentInspectorTaskInfoBinding
+import com.petproject.workflow.presentation.viewmodels.InspectorTaskInfoViewModel
+import com.petproject.workflow.presentation.viewmodels.ViewModelFactory
+import com.petproject.workflow.presentation.views.adapters.EmployeeInfoViewHolder
+import javax.inject.Inject
 
 class InspectorTaskInfoFragment : Fragment() {
+    private var _binding: FragmentInspectorTaskInfoBinding? = null
+    private val binding: FragmentInspectorTaskInfoBinding get() = _binding!!
+
+    private val args by navArgs<InspectorTaskInfoFragmentArgs>()
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel by lazy {
+        ViewModelProvider.create(
+            viewModelStore,
+            viewModelFactory
+        )[InspectorTaskInfoViewModel::class]
+    }
+
+    private val component by lazy {
+        (requireActivity().application as WorkFlowApplication)
+            .component
+            .activityComponentFactory()
+            .create(args.taskId)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inspector_task_info, container, false)
+    ): View {
+        component.inject(this)
+        _binding = FragmentInspectorTaskInfoBinding.inflate(inflater, container, false)
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.inspectorTask.observe(viewLifecycleOwner) {
+            it.executor?.let { executor ->
+                val employeeInfoViewHolder = EmployeeInfoViewHolder(binding.taskExecutor)
+                employeeInfoViewHolder.bind(executor) {}
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
